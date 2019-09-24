bed889b2f0350f832ba365bff44bf40a82e7dc27
mikeacosta <mike@acosta.net>
2019-09-23 22:05:48 -0700

    modify search

--- src/SearchApi.py
+++ src/SearchApi.py
@@ -12,12 +12,32 @@ url = os.environ['url']
 
 def lambda_handler(event, context):
 
+    titlesOnly = False
+
+    try:
+        searchTerm = event['queryStringParameters']['q']
+    except:
+        searchTerm = ''
+
+    try:
+        if event['queryStringParameters']['t'] == '1':
+            fields = ["Title"]
+            titlesOnly = True
+            if len(searchTerm) >= 2:
+                searchTerm = '*' + searchTerm + '*' 
+            else:
+                searchTerm = ''
+        else:
+            fields = ["Title","Description"]
+    except:
+        fields = ["Title","Description"]
+
     query = {
         "size": 25,
         "query": {
-            "multi_match": {
-               "query": event['queryStringParameters']['q'],
-                "fields": ["Title","Description"]
+            "query_string": {
+               "query": searchTerm,
+                "fields": fields
             }
         }
     }
@@ -31,7 +51,12 @@ def lambda_handler(event, context):
     searchResults = []
 
     for item in hits:
-        searchResults.append(item['_source'])
+        if titlesOnly == True:
+            result = json.dumps(item['_source'])
+            title = json.loads(result)
+            searchResults.append(title.get('Title'))
+        else:
+            searchResults.append(item['_source'])
 
     response = {
         "statusCode": 200,
@@ -42,5 +67,6 @@ def lambda_handler(event, context):
         "body": json.dumps(searchResults)
     }        
 
+    print("search term: " + searchTerm)
     print(json.dumps(response))
     return response
